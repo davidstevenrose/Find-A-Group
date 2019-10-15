d534781bcf3d7b17fb3c7578cfc43604bf93baba
brien17 <43622552+brien17@users.noreply.github.com>
2019-10-14 22:47:25 -0400

    added login screen fxml and controller, added primary screen fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,107 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+  @FXML private TitledPane title;
+
+  @FXML private Button searchGroupsButton;
+
+  @FXML private ChoiceBox<String> searchTag1;
+
+  @FXML private ChoiceBox<String> searchTag3;
+
+  @FXML private ChoiceBox<String> searchTag4;
+
+  @FXML private ChoiceBox<String> searchTag2;
+
+  @FXML private Button joinGroupsButton;
+
+  @FXML private TableView<?> searchGroupTable;
+
+  @FXML private TableColumn<?, ?> searchGroupsGroupNameCol;
+
+  @FXML private TableColumn<?, ?> searchGroupsDescriptionCol;
+
+  @FXML private Tab meetingsGroupNameCol;
+
+  @FXML private TextField searchLocationTextbox;
+
+  @FXML private DatePicker searchDataPicker;
+
+  @FXML private Button searchMeetingsDateLocationButton;
+
+  @FXML private Button searchByGroupsButton;
+
+  @FXML private ChoiceBox<String> groupsPicker;
+
+  @FXML private TableView<String> findMeetingsTable;
+
+  @FXML private TableColumn<?, ?> meetingsDateCol;
+
+  @FXML private TableColumn<?, ?> meetingsTimeCol;
+
+  @FXML private TableColumn<?, ?> meetingsLocationCol;
+
+  @FXML private Tab add;
+
+  @FXML private ChoiceBox<String> addTag1;
+
+  @FXML private ChoiceBox<String> addTag2;
+
+  @FXML private ChoiceBox<String> addTag3;
+
+  @FXML private ChoiceBox<String> addTag4;
+
+  @FXML private DatePicker addMeetingDatePicker;
+
+  @FXML private TextField addMeetingLocationTextfield;
+
+  @FXML private Button saveChanges;
+
+  @FXML private TextArea groupDescription;
+
+  @FXML private ComboBox<?> addMeetingTimePicker;
+
+  @FXML private ChoiceBox<String> editGroupSelector;
+
+  @FXML private TextField createGroupTextfield;
+
+
+
+  @FXML
+  public void initialize() {
+    /*
+    // Adding values to they type choice box
+    for (ItemType it : ItemType.values()) {
+        productType.getItems().add(it);
+    }
+
+    // Adding numbers to the quantity selector
+    for (int i = 1; i < 11; i++) {
+        String number = "" + i;
+        chooseQuantity.getItems().add(number);
+    }
+    chooseQuantity.getSelectionModel().selectFirst();
+    chooseQuantity.setEditable(true);
+    testMultimedia();
+    connectToDatabase();
+    populateProductLine();
+    */
+
+  }
+    @FXML
+    void joinGroupButtonClick(MouseEvent event) {
+    }
 }
---
+++ src/sample/loginController.java
@@ -0,0 +1,52 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class loginController {
+
+  @FXML private PasswordField passwordField;
+
+  @FXML private TextField usernameField;
+
+  @FXML private Button loginButton;
+
+  @FXML private Button createAccountButton;
+
+  @FXML private Label errorLabel;
+
+  @FXML private Label confirmPasswordLabel;
+
+  @FXML private PasswordField confirmPasswordField;
+
+  /**
+   * This method allows the user to log into their account and will change the scene to the primary
+   * scene
+   *
+   * @param event
+   */
+  @FXML
+  void loginClicked(MouseEvent event) throws IOException {
+    // Creating the new scene
+    Parent primaryScreenParent = FXMLLoader.load(getClass().getResource("PrimaryScreen.fxml"));
+    Scene primaryScreen = new Scene(primaryScreenParent);
+
+    // Getting the stage
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+    window.setScene(primaryScreen);
+    window.show();
+  }
+}
