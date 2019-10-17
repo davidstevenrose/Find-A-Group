d48e71ba6f34a07f7dad62413880722461135afb
anagalacticRuby <841b2dRuby>
2019-10-17 01:34:17 -0400

    Added some VERY basic GUI elements and played around with some potential ideas.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
 public class Controller {
+
+    @FXML
+    private Tab tabLogin;
+
+    @FXML
+    private TextField userNAME;
+
+    @FXML
+    private TextField passWORD;
+
+    @FXML
+    private ToggleButton daENTERbutton;
+
 }
