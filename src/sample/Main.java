22f08d3e98936aee7be21267ed97cb442f4f60f8
David <dsrose3626@eagle.fgcu.edu>
2019-10-18 14:51:51 -0400

    Added a header in Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The class that holds the main method.
+ * <h1>Some Thoughts</h1>
+ * <p>Our SRS specifies that we must direct the client to the profile page after logon,
+ * but we have no fxml for the profile page (Statement 13). We also must revisit the 'group finding'
+ * mechanism the client will be using. I.E. should the user only search with up to four group tags?
+ *
+ * </p>
+ */
 public class Main extends Application {
 
     private String fieldOne;
@@ -17,8 +26,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 675, 600));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
