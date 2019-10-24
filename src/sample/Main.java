7575a212de39bb5a5b18fbe24ab3b3960edc2e9d
anagalacticRuby <841b2dRuby>
2019-10-24 02:18:09 -0400

    Added 2 Stylesheets just to flex and show off some color theme ideas.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The class that holds the main method.
+ * <h1>Some Thoughts</h1>
+ * <p>Our SRS specifies that we must direct the client to the profile page after logon,
+ * but we have no fxml for the profile page (Statement 13). We also must revisit the 'group finding'
+ * mechanism the client will be using. I.E. should the user only search with up to four group tags?
+ *
+ * </p>
+ */
 public class Main extends Application {
 
     private String fieldOne;
@@ -13,12 +22,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("2sday's Find A Group");
+        primaryStage.setScene(new Scene(root, 675, 600));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
