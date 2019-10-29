160a9aa49d2bc625314e08609a2632f0da938c7c
ZStrikeXIII <jacksonturner1999@gmail.com>
2019-10-29 15:53:12 -0400

    Fixed bug in edit groups method

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,20 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The class that holds the main method.
+ * <h1>Some Thoughts</h1>
+ * <p>Our SRS specifies that we must direct the client to the profile page after logon,
+ * but we have no fxml for the profile page (Statement 13). We also must revisit the 'group finding'
+ * mechanism the client will be using. I.E. should the user only search with up to four group tags?
+ *
+ * </p>
+ */
 public class Main extends Application {
-    //Jackson's Files
 
     private String fieldOne;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MeetingDetails.fxml"));
+        primaryStage.setTitle("2sday's Find A Group");
+        primaryStage.setScene(new Scene(root, 675, 600));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
---
+++ src/sample/MeetDetController.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class MeetDetController {
+
+  //Need to make the program able to go to the Meeting Details Screen.
+
+  boolean meetingAttend = false;
+
+  @FXML
+  public void attendMeetingClicked(MouseEvent mouseEvent) {
+    meetingAttend = true;
+    System.out.printf("Meeting Attended");
+  }
+
+  public void cancelAttendanceClicked(MouseEvent mouseEvent) {
+    if (meetingAttend == true) {
+      meetingAttend = false;
+      System.out.println("Attendance Canceled");
+    } else {
+      System.out.println("You cannot cancel attendance to a meeting you aren't already attending");
+    }
+  }
+}
