7dacfcd12365b79c9cd56621c8253cd6fdffb0c9
anagalacticRuby <841b2dRuby>
2019-11-06 18:31:44 -0500

    Current Version of Master with some CSS and JAVAFX edits. Files Changed: PrimaryScreen.fxml Style.Css

---
+++ src/io/github/davidstevenrose/Main.java
@@ -0,0 +1,34 @@
+package io.github.davidstevenrose;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The class that holds the main method.
+ * <h1>Some Thoughts</h1>
+ * <p>Our SRS specifies that we must direct the client to the profile page after logon,
+ * but we have no fxml for the profile page (Statement 13). We also must revisit the 'group finding'
+ * mechanism the client will be using. I.E. should the user only search with up to four group tags?
+ *
+ * </p>
+ */
+public class Main extends Application {
+
+    private String fieldOne;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../../../../../Deleted Java IDE Stuff/sample/sample.fxml"));
+        primaryStage.setTitle("2sday's Find A Group");
+        primaryStage.setScene(new Scene(root, 675, 600));
+        primaryStage.show();
+        
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
