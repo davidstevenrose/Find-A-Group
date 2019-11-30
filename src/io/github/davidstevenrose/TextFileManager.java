a02c403fe0bd1277186cfda78bf855ecc42ff1d6
David <dsrose3626@eagle.fgcu.edu>
2019-11-30 15:31:24 -0500

    Merge Cameron into David Fixed a bug where someone added null to the tag boxes completely breaking all search functionality for searching for groups. Replaced null with "" in all tag boxes. Also, selected first element on tag boxes by default so users can search without using all 4 boxes.

--- src/io/github/davidstevenrose/TextFileManager.java
+++ src/io/github/davidstevenrose/TextFileManager.java
@@ -108,9 +108,10 @@ public class TextFileManager {
       String loc = meeting.getLocation();
       String time = meeting.getTime();
       String groupName = meeting.getGroupName();
+      String hostName = meeting.getHostName();
 
       // Writing to file
-      print.write(date + "," + loc + "," + time + "," + groupName + "," );
+      print.write(date + "," + loc + "," + time + "," + groupName + "," + hostName + ",");
       for (String attendee : meeting.getAttendees()) {
         print.write(attendee + ",");
       }
